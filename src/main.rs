1cef8886cad3cb3714f023114fe69b7644c31453
梶塚太智 <kajizukataichi@outlook.jp>
2024-08-22 12:01:42 +0900

    Modify to use clap

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,49 @@ use std::io;
 mod gitl;
 mod info;
 mod install;
+use clap::{Arg, Command};
 
 fn main() {
     let version = info::VERSION;
-    let _args: Vec<String> = env::args().collect();
-    if _args.len() > 1 {
-        if _args[1] == "update" {
+
+    let matches = Command::new("knife")
+        .about("A simple, fast, and safe package manager")
+        .arg(
+            Arg::new("command")
+                .help("The command to execute")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::new("package")
+                .help("The package name (for install command)")
+                .required(false)
+                .index(2),
+        )
+        .get_matches();
+
+    match matches.get_one::<String>("command").unwrap().as_str() {
+        "update" => {
             gitl::Gitl::update_package_list();
             std::process::exit(0);
-        } else if _args[1] == "upgrade" {
-            gitl::Gitl::upgrade_knife(version);
-        } else if _args[1] == "install" && _args.len() == 3 {
-            install::Get::search_program(_args[2].to_string());
-        } else if _args[1] == "install" {
-            eprintln!("{} Specify the package to install", "Error:".red(),);
         }
-    } else {
-        println!(
-            "{}{}",
-            "Error: option needed.".red(),
-            " Please run --help option and check your options"
-        );
-        std::process::exit(1);
+        "upgrade" => {
+            gitl::Gitl::upgrade_knife(version);
+        }
+        "install" => {
+            if let Some(package) = matches.get_one::<String>("package") {
+                install::Get::search_program(package.to_string());
+            } else {
+                eprintln!("{} Specify the package to install", "Error:".red());
+            }
+        }
+        _ => {
+            println!(
+                "{}{}",
+                "Error: option needed.".red(),
+                " Please run with the --help option to check your options."
+            );
+            std::process::exit(1);
+        }
     }
 }
