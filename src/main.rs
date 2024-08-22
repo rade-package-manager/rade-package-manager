0d1a3b5fe906a6aaeb8e3be4d3a6f15c7b13b574
kimuti-tsukai <ruster354@gmail.com>
2024-08-22 17:07:36 +0900

    Change clap to use Parser

--- src/main.rs
+++ src/main.rs
@@ -8,49 +8,33 @@ use std::io;
 mod gitl;
 mod info;
 mod install;
-use clap::{Arg, Command};
+use clap::Parser;
+
+#[derive(Parser)]
+/// A simple, fast, and safe package manager
+enum Cli {
+    Update,
+    Upgrade,
+    Install {
+        /// The package name (for install command)
+        package: String
+    },
+}
 
 fn main() {
     let version = info::VERSION;
 
-    let matches = Command::new("knife")
-        .about("A simple, fast, and safe package manager")
-        .arg(
-            Arg::new("command")
-                .help("The command to execute")
-                .required(true)
-                .index(1),
-        )
-        .arg(
-            Arg::new("package")
-                .help("The package name (for install command)")
-                .required(false)
-                .index(2),
-        )
-        .get_matches();
+    let args = Cli::parse();
 
-    match matches.get_one::<String>("command").unwrap().as_str() {
-        "update" => {
+    match args {
+        Cli::Update => {
             gitl::Gitl::update_package_list();
-            std::process::exit(0);
         }
-        "upgrade" => {
+        Cli::Upgrade => {
             gitl::Gitl::upgrade_knife(version);
         }
-        "install" => {
-            if let Some(package) = matches.get_one::<String>("package") {
-                install::get::search_program(package.to_string());
-            } else {
-                eprintln!("{} Specify the package to install", "Error:".red());
-            }
-        }
-        _ => {
-            println!(
-                "{}{}",
-                "Error: option needed.".red(),
-                " Please run with the --help option to check your options."
-            );
-            std::process::exit(1);
+        Cli::Install { package } => {
+            install::get::search_program(package)
         }
     }
 }
