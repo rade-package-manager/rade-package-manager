0861a9dc3ceb40aab60a73f45abd4f445913bc5d
梶塚太智 <kajizukataichi@outlook.jp>
2024-08-22 17:29:35 +0900

    Modify to use subcommand

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,21 @@ fn main() {
     let version = info::VERSION;
 
     let matches = Command::new("knife")
+        .version("0.2")
         .about("A simple, fast, and safe package manager")
-        .arg(
-            Arg::new("command")
-                .help("The command to execute")
-                .required(true)
-                .index(1),
-        )
-        .arg(
-            Arg::new("package")
-                .help("The package name (for install command)")
-                .required(false)
-                .index(2),
+        .subcommand(Command::new("update").about("Update the package list"))
+        .subcommand(Command::new("upgrade").about("Upgrade the knife tool"))
+        .subcommand(
+            Command::new("install").about("Install a package").arg(
+                Arg::new("package")
+                    .help("The package to install")
+                    .required(true)
+                    .index(1),
+            ),
         )
         .get_matches();
 
+<<<<<<< Updated upstream
     match matches.get_one::<String>("command").unwrap().as_str() {
         "update" => {
             gitl::update_package_list();
@@ -41,6 +41,19 @@ fn main() {
         "install" => {
             if let Some(package) = matches.get_one::<String>("package") {
                 install::install_program(&package.to_string());
+=======
+    match matches.subcommand() {
+        Some(("update", _)) => {
+            gitl::Gitl::update_package_list();
+            std::process::exit(0);
+        }
+        Some(("upgrade", _)) => {
+            gitl::Gitl::upgrade_knife(version);
+        }
+        Some(("install", sub_matches)) => {
+            if let Some(package) = sub_matches.get_one::<String>("package") {
+                install::get::search_program(package.to_string());
+>>>>>>> Stashed changes
             } else {
                 eprintln!("{} Specify the package to install", "Error:".red());
             }
@@ -48,8 +61,8 @@ fn main() {
         _ => {
             println!(
                 "{}{}",
-                "Error: option needed.".red(),
-                " Please run with the --help option to check your options."
+                "Error: command needed.".red(),
+                " Please run with the --help option to check your commands."
             );
             std::process::exit(1);
         }
